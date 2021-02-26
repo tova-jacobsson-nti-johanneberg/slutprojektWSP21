5e60f81e304d44b5c640b62672199ea1b8756c89
tova-jacobsson-nti-johanneberg <tova.jacobsson@elev.ga.ntig.se>
2021-02-26 13:37:35 +0100

    emil test
    registrera och logga in

--- app.rb
+++ app.rb
@@ -6,12 +6,51 @@ require 'bcrypt'
 
 get('/') do
     slim(:index)
-  end
+end
 
-  get('/login') do
+get('/login') do
     slim(:login)
-  end
+ end
 
-  get('/register') do
+get('/register') do
     slim(:register)
-  end
+end
+
+post('/login') do 
+    username = params[:username]
+    password = params[:password]
+    db = SQLite3::Database.new('databas/databas.db')
+    db.results_as_hash = true 
+    result = db.execute("SELECT * FROM users WHERE username = ?", username).first
+    pwdigest = result["pwdigest"]
+    id = result["id"]
+  
+    if BCrypt::Password.new(pwdigest) == password
+      session[:id] = id
+      redirect('/')
+    else 
+      "FEL LÖSEN!"
+    end 
+  end 
+  
+  get('/handla') do 
+    id = session[:id].to_i
+    db = SQLite3::Database.new('databas/databas.db')
+    db.results_as_hash = true 
+    result = db.execute("SELECT * FROM  WHERE paket_id = ?", id)
+    slim(:"/handla",locals:{paket:result})
+  end 
+  
+  post('/users/new') do
+    username = params[:username]
+    password = params[:password]
+    password_confirm = params[:password_confirm]
+    if (password == password_confirm)
+      password_digest = BCrypt::Password.create(password)
+      db = SQLite3::Database.new('databas/databas.db')
+      db.execute("INSERT INTO users (username,pwdigest) VALUES (?,?)",username,password_digest)
+      redirect('/')
+    else 
+      "Lösenorden matchade inte"
+    end 
+  end 
